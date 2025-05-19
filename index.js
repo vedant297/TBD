fe3076b431779703f033e6e7d8f0e58dc8e30f67
Vedant Patel <vedantpatel297@gmail.com>
2025-05-19 14:38:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,6 +70,9 @@ passport.deserializeUser(User.deserializeUser())
 
 const dbUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/travel';
 
+console.log('DB URL below')
+console.log(dbUrl)
+
 mongoose.connect( dbUrl , { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connection Open')
